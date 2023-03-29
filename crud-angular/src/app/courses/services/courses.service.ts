96d45648ae8ed164cca10418d694b7ec8884139f
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-03-29 20:55:05 -0300

    Criar Curso (HTTP POST)

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -8,7 +8,7 @@ import { delay, first, tap } from 'rxjs/operators';
 })
 export class CoursesService {
 
-  private readonly API = '/api/courses';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
--- crud-spring/src/main/java/com/starking/crudspring/controllers/CourseController.java
+++ crud-spring/src/main/java/com/starking/crudspring/controllers/CourseController.java
@@ -2,9 +2,11 @@ package com.starking.crudspring.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,7 +28,8 @@ public class CourseController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Course> save() {
-		return ResponseEntity.accepted().build();
+	public ResponseEntity<Course> save(@RequestBody Course course) {
+		Course courseEntity = this.service.save(course);
+		return ResponseEntity.status(HttpStatus.CREATED).body(courseEntity);
 	}
 }
