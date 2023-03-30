f4c66523463fd70a9ad4afa77306285df9086c49
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-03-30 20:03:44 -0300

    Typed Forms

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from './../services/courses.service';
@@ -12,19 +12,20 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CourseFormComponent implements OnInit {
 
-  form: FormGroup;
+  form = this.formBuilder.group({
+    name: ['', Validators.required],
+    category : ['', Validators.required]
+  });
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
     private _snackBar: MatSnackBar,
     private location: Location) {
-    this.form = formBuilder.group( {
-      name: [null],
-      category : [null]
-    });
+
   }
 
   ngOnInit(): void {
+    this.form.value;
   }
 
   onSubmit() {
