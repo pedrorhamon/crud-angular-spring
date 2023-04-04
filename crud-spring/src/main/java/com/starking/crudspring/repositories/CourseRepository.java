081e2699637780dc07345cb9bba68febaf64793a
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-03 21:59:11 -0300

    Organizando imports do back

--- crud-spring/src/main/java/com/starking/crudspring/repositories/CourseRepository.java
+++ crud-spring/src/main/java/com/starking/crudspring/repositories/CourseRepository.java
@@ -4,6 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.starking.crudspring.model.Course;
 
-public interface CourseRepository extends JpaRepository<Course, Long>{
-
-}
+public interface CourseRepository extends JpaRepository<Course, Long>{}
